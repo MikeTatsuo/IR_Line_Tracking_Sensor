9c0ca281f1bae48fa354d4933c976a6492b26a96
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-02-08 19:49:40 -0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define DIGITAL_PIN 2
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("* IR Line Tracking *");
+}
+
+void loop() {
+  int blocked = digitalRead(DIGITAL_PIN);
+
+  lcd.setCursor(0, 2);
+
+  if (blocked == LOW) {
+    lcd.print("blocked");
+  } else {
+    lcd.print("free    ");
+  }  
+}
